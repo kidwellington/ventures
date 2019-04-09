f55e683028359cc34116512320e1446f65a95744
Jason Kidwell <jasonskidwell@gmail.com>
2019-04-08 21:40:14 -0400

    Contact Us: get content by Page id

--- src/app/ventures/contact/contact.component.ts
+++ src/app/ventures/contact/contact.component.ts
@@ -10,19 +10,33 @@ import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
 })
 export class ContactComponent implements OnInit {
 
-  public contacts: Entry<any>[];
   public page: Entry<any>;
+  public contacts: Entry<any>[];
+  public address: any;
 
   constructor(private contentfulService: ContentfulService) { }
 
   ngOnInit() {
     this.contentfulService.cdaClient.getEntries({
-      content_type: 'page'
+      content_type: 'page',
+      'sys.id': '65noSk5axOZNDFI3wLhec'
     }).then(page => {
       this.page = page.items[0];
-      console.log(this.page)
-      // Quick implementation. Change this to get the Page content type, similar to the 
-      // Venture Investing page
+      console.log(this.page);
+    })
+
+    this.contentfulService.cdaClient.getEntries({
+      content_type: 'contact'
+    }).then(contacts => {
+      this.contacts = contacts.items;
+      console.log(this.contacts);
+      
+      // TODO: getting parsed html here, but i need this
+      // returned as an array so i can iterate over it
+      this.contacts.forEach((contact, i) => {
+        this.address = documentToHtmlString(contact.fields.address1);
+        console.log(this.address)
+      });
     })
   }
 
