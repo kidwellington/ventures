63b26b658a17c1851fb7b4b89fc78c71d82347c4
John Gibison Jr <johnny.gibison@gmail.com>
2019-05-02 10:15:11 -0400

    Filtering portfolio page

--- src/app/ventures/investing/portfolio/portfolio.component.ts
+++ src/app/ventures/investing/portfolio/portfolio.component.ts
@@ -9,9 +9,12 @@ import { ContentfulService } from 'src/app/services/contentful.service';
 })
 export class PortfolioComponent implements OnInit {
   public partners: Entry<any>[];
+  public selectedCategory: number;
   public pills: string[];
+  public pillSections: boolean[];
 
   constructor(private contentfulService: ContentfulService) {
+    this.selectedCategory = 0;
   }
 
   ngOnInit() {
@@ -30,7 +33,25 @@ export class PortfolioComponent implements OnInit {
       .filter((partner, index, self) => {
         return self.indexOf(partner) === index;
       });
+
+      // create the pill Section array
+      this.pillSections = this.pills
+      .map(() => {
+        return false;
+      });
+
       this.pills.unshift('All Companies');
-    })
+      this.pillSections.unshift(true);
+    });
+  }
+
+  public filterCategories(pillIndex: number) {
+    this.pillSections[this.selectedCategory] = false;
+    this.pillSections[pillIndex] = true;
+    this.selectedCategory = pillIndex;
+  }
+
+  public isSectionVisible(sectionIndex: number) {
+    return this.pillSections[sectionIndex] || this.pillSections[0];
   }
 }
